87dd253b122a8bcbc27e6a4fd85648b0e8c4ba41
JUAN DAVID PRIETO VELASQUEZ <jprietod@uninorte.edu.co>
2025-03-21 15:53:28 -0500

    Se creo la clase Dog

---
+++ src/core/Dog.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author jprietod
+ */
+public class Dog {
+    private String name;
+    private int age;
+    private String race;
+    private String owner;
+
+    public Dog(String name, int age, String race, String owner) {
+        this.name = name;
+        this.age = age;
+        this.race = race;
+        this.owner = owner;
+    }
+    
+    
+    
+}
