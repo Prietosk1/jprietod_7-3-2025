23b6ee7c76c083f63f3c40eba5411b35a748f4a2
JUAN DAVID PRIETO VELASQUEZ <ddbarraza@uninorte.edu.co>
2025-03-28 15:17:12 -0500

    Se cambio el metodo toString de la clase Dog y se imprimio un objeto de este en Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@
  */
 package main;
 
+import core.Dog;
 import core.Person;
 
 /**
@@ -14,8 +15,10 @@ public class Main {
 
     public static void main(String[] args) {
         Person persona1 = new Person(1, "Juan",19);
-        
         System.out.println(persona1);
+        
+        Dog dog = new Dog("Oscar", 2, "Canino", "Juan");
+        System.out.println(dog);
     }
 
 }
